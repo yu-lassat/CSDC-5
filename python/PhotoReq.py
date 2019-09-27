0297041144636648f0216152fdfc85ed1a979989
Chester <43485962+c-git@users.noreply.github.com>
2019-09-27 19:21:15 -0400

    Format Code
    - Set two spaces before class as per PEP 8
    - Break lines longer than 80

--- python/PhotoReq.py
+++ python/PhotoReq.py
@@ -1,16 +1,19 @@
 import datetime
 
+
 class PhotoReq:
     def __init__(self, id: int, date: datetime.datetime, camera_id: int):
         self._id = id
         self._date = date
         self._camera_id = camera_id
         self._is_complete = False
-   
+
     def __eq__(self, other):
         return self._id == other._id and self._date == other._date \
-        and self._camera_id == other._camera_id and self._is_complete == other._is_complete
-    
+               and self._camera_id == other._camera_id and self._is_complete \
+               == other._is_complete
+
     def same_id_diff_time_or_camera(self, other):
-        return (not(self._date == other._date \
-                and self._camera_id == other._camera_id) and self._id == other._id)
+        return (not (self._date == other._date \
+                     and self._camera_id == other._camera_id) and self._id
+                == other._id)
