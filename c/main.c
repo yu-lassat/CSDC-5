16778e85f9bb36c73584fa2c8a5ae77e15731ff9
Chester <43485962+c-git@users.noreply.github.com>
2019-09-29 01:28:28 -0400

    Setup test loop

--- c/main.c
+++ c/main.c
@@ -2,6 +2,7 @@
 #pragma argsused
 
 #include <stdio.h>
+#include "comms.h"
 
 #ifdef _WIN32
 #include <tchar.h>
@@ -11,5 +12,22 @@ typedef char _TCHAR;
 #endif
 
 int _tmain(int argc, _TCHAR* argv[]) {
-	return 0;
+    /* TODO 2 -cComms : Check incoming file for info */
+    /* TODO 3 -cData Structure : Track Pending Requests */
+    /* TODO 3 -cData Structure : Track Which Requests Are not confirmed */
+    /* TODO 4 -cError Handling : Check if request is pending for too long
+     without confirmation */
+    /* TODO 2 : Start Python Script */
+    /* TODO 4 : Confirm the python program started correctly */
+    char received[100] = ""; /* TODO 4 : Remove variable used for testing */
+    while (strcmp(received, "exit")) {
+        printf("Enter number of seconds to ofset time by: ");
+        gets(received);
+        /* TODO 1 : Change value into a time offset from now and send to python */
+        CommsWrite(received);
+    }
+
+    CommsFree();
+
+    return 0;
 }
